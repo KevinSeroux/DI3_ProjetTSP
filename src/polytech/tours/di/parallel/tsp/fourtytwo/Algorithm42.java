57f071dedfba4652865780ab3a7890d9c866769a
Kévin Seroux <kevin.seroux@gmail.com>
2016-05-13 16:56:52 +0200

    Import

---
+++ src/polytech/tours/di/parallel/tsp/fourtytwo/Algorithm42.java
@@ -0,0 +1,15 @@
+package polytech.tours.di.parallel.tsp.fourtytwo;
+
+import java.util.Properties;
+
+import polytech.tours.di.parallel.tsp.Algorithm;
+import polytech.tours.di.parallel.tsp.Solution;
+
+public class Algorithm42 implements Algorithm {
+
+	@Override
+	public Solution run(Properties config) {
+		return null;
+	}
+
+}
