576c658caf043d876ea824d3fbdc15d493d8e656
Kévin Seroux <kevin.seroux@gmail.com>
2016-05-19 12:58:35 +0200

    serial run() code done

--- src/polytech/tours/di/parallel/tsp/fourtytwo/Algorithm42.java
+++ src/polytech/tours/di/parallel/tsp/fourtytwo/Algorithm42.java
@@ -3,13 +3,37 @@ package polytech.tours.di.parallel.tsp.fourtytwo;
 import java.util.Properties;
 
 import polytech.tours.di.parallel.tsp.Algorithm;
+import polytech.tours.di.parallel.tsp.Instance;
+import polytech.tours.di.parallel.tsp.InstanceReader;
 import polytech.tours.di.parallel.tsp.Solution;
 
 public class Algorithm42 implements Algorithm {
 
 	@Override
 	public Solution run(Properties config) {
-		return null;
+		Solution finalSolution = null;
+		
+		//Build the instance from the config file
+		InstanceReader ir = new InstanceReader();
+		ir.buildInstance(config.getProperty("instance"));
+		Instance instance = ir.getInstance();
+
+		//Manage the timing (*1000 to have the time in millisec)
+		long timeMax = Long.valueOf(config.getProperty("maxcpu")) * 1000;
+		long startTime = System.currentTimeMillis();
+		while((System.currentTimeMillis() - startTime) < timeMax) {
+			
+			//Here we go
+			Solution randomSolution = generateRandomSolution();
+			Solution locallySearchedSolution = localSearch(randomSolution);
+			
+			if(locallySearchedSolution.getOF() > randomSolution.getOF())
+				finalSolution = locallySearchedSolution;
+			else
+				finalSolution = randomSolution;
+		}
+		
+		return finalSolution;
 	}
 	
 	/** TODO: Kevin
@@ -17,6 +41,8 @@ public class Algorithm42 implements Algorithm {
 	 * @return the random solution
 	 */
 	private Solution generateRandomSolution() {
+		
+		
 		return null;
 	}
 
