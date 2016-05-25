db4bcb29d8503308d1798b1e3c53878c7c113a09
Kévin Seroux <kevin.seroux@gmail.com>
2016-05-25 20:55:56 +0200

    Parallel version (with local research non-interruptible)

--- src/polytech/tours/di/parallel/tsp/fourtytwo/Algorithm42.java
+++ src/polytech/tours/di/parallel/tsp/fourtytwo/Algorithm42.java
@@ -1,158 +1,88 @@
 package polytech.tours.di.parallel.tsp.fourtytwo;
 
-import java.util.Collections;
+import java.util.List;
+import java.util.ArrayList;
 import java.util.Properties;
-import java.util.Random;
+import java.util.concurrent.Callable;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 import polytech.tours.di.parallel.tsp.Algorithm;
 import polytech.tours.di.parallel.tsp.Instance;
 import polytech.tours.di.parallel.tsp.InstanceReader;
 import polytech.tours.di.parallel.tsp.Solution;
-import polytech.tours.di.parallel.tsp.TSPCostCalculator;
 
 public class Algorithm42 implements Algorithm {
-	private Random randomizer; //TODO: Use parallel version
-	private Instance instance;
+	long seed, timeMax;
+	Instance instance;
 
 	@Override
 	public Solution run(Properties config) {
-		Solution finalSolution = null;
-		
-		//Get some parameters
-		randomizer = new Random(Long.valueOf(config.getProperty("seed")));
 		
 		//Build the instance from the config file
 		InstanceReader ir = new InstanceReader();
 		ir.buildInstance(config.getProperty("instance"));
 		instance = ir.getInstance();
 
+		seed = Long.valueOf(config.getProperty("seed"));
+
 		//Manage the timing (*1000 to have the time in millisec)
-		long timeMax = Long.valueOf(config.getProperty("maxcpu")) * 1000;
-		long startTime = System.currentTimeMillis();
-		while((System.currentTimeMillis() - startTime) < timeMax) {
-			
-			//Find the solutions
-			Solution generatedSolution = generateRandomSolution();
-			System.out.println("Random: " + generatedSolution);
-			
-			Solution locallySearchedSolution = localSearch(generatedSolution);
-			System.out.println("Locally searched solution: " + locallySearchedSolution);
-			
-			if(finalSolution == null)
-				finalSolution = generatedSolution;
-			
-			if(locallySearchedSolution.getOF() < finalSolution.getOF())
-				finalSolution = locallySearchedSolution;
-			
-			System.out.println("---------------------------------------");
-		}
+		timeMax = Long.valueOf(config.getProperty("maxcpu"));
 		
-		System.out.println("---------------------------------------");
-		
-		return finalSolution;
+		return executeThreads();
 	}
 	
-	/** Generate and return only one random solution
-	 * @return the random solution
-	 */
-	private Solution generateRandomSolution() {
-		Solution solution = new Solution();
+	public Solution executeThreads() {
 		
-		/* We add all the city (index) to the solution,
-		 * no more things are required. TSPCostCalculator
-		 * do the trick */
-		for(int i = 0; i < instance.getN(); i++) {
-			solution.add(i);
-		}
+		//TODO: Manage threads interruption
+		int nbThreads = Runtime.getRuntime().availableProcessors();
+		List<Solution> solutions = new ArrayList<Solution>(nbThreads);
+		List<Callable<Object>> threads = new ArrayList<Callable<Object>>(nbThreads);
+		final ScheduledExecutorService executor = Executors.newScheduledThreadPool(nbThreads + 1);
 		
-		//Randomize the solution indices (cities)
-		Collections.shuffle(solution, randomizer);
-
-		//Compute the efficiency of the solution
-		solution.setOF(TSPCostCalculator.calcOF(instance, solution));
-		
-		return solution;
-	}
-
-	/* Return the best solution among many changed solution
-	 * (local search algorithm)
-	 * @param generatedSolution The solution to begin with
-	 * @return the best solution found with the algorithm,
-	 * null if no better solution
-	 */
-	private Solution localSearch(Solution solution) {
-		boolean continueExploration = true;
-		Solution bestSolution = solution;
-		
-		while(continueExploration)
-		{
-			Solution swapSolution;
-			
-			swapSolution = exploreNeighborhood(bestSolution);
-			//TODO: Solve this, computeSwapCost is inaccurate
-			if((float)swapSolution.getOF() < (float)bestSolution.getOF())
-				bestSolution = swapSolution;
-			else
-				continueExploration = false;
-		}
-		
-		return bestSolution;
-	}
-
-	/* Return the best solution among many changed solution
-	 * (local search algorithm)
-	 * @param generatedSolution The solution to begin with
-	 * @return the best solution found with the algorithm
-	 */
-	private Solution exploreNeighborhood(Solution solution) {
-		Solution bestSolution = solution;
-		Solution swapSolution = solution.clone();
-
-		for(int i = 0; i < solution.size(); i++)
-		{
-			for(int j = i + 1; j < solution.size(); j++)
-			{
-				double costBefore = swapSolution.getOF();
-
-				double relativeCostBefore = computeSwapCost(swapSolution, i, j);
-				swapSolution.swap(i, j);
-				double relativeCostAfter = computeSwapCost(swapSolution, i, j);
-				double diffCost = relativeCostBefore - relativeCostAfter;
-				
-				swapSolution.setOF(costBefore - diffCost);
-				//swapSolution.setOF(TSPCostCalculator.calcOF(instance, swapSolution));
-				
-				if(swapSolution.getOF() < bestSolution.getOF()) 
-					bestSolution = swapSolution.clone();
+		//Thread that cancel all the other one after the timeout
+		executor.schedule(new Runnable() {
+			public void run() {
+				executor.shutdownNow();
 			}
+		}, timeMax, TimeUnit.SECONDS);
+		
+		long startTime = System.currentTimeMillis();
+		
+		//Threads that compute
+		for(int i = 0; i < nbThreads; i++) {
+			Solution currentSolution = new Solution();
+			solutions.add(currentSolution);
+			
+			ThreadedSolutionFinder solutionFinder =
+					new ThreadedSolutionFinder(seed, instance, currentSolution);
+			
+			threads.add(Executors.callable(solutionFinder));
+		}
+		
+		/* Execute and then wait that all threads have finished */
+		try {
+			executor.invokeAll(threads);
+
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+			return null;
+		}
+		
+		long time = System.currentTimeMillis() - startTime;
+		System.out.println("---------------------------------------");
+		System.out.println("All tasks terminated in " + time / 1000.f + "s");
+		
+		//Iterate over all the found solutions
+		Solution bestSolution = solutions.get(0);
+		for(int i = 1; i < nbThreads; i++) {
+			Solution currentSolution = solutions.get(i);
+			if(currentSolution.getOF() < bestSolution.getOF())
+				bestSolution = currentSolution;
 		}
 		
 		return bestSolution;
-	}
-	
-	private double computeSwapCost(Solution solution, int i, int j) {
-		double cost;
-		int locFrom, locTo;
-		int n = solution.size();
 		
-		locFrom = solution.get((i - 1 + n) % n);
-		locTo = solution.get(i);
-		cost = instance.getDistance(locFrom, locTo);
-		
-		locFrom = solution.get(i);
-		locTo = solution.get(i + 1);
-		cost += instance.getDistance(locFrom, locTo);
-
-		if(j - i != 1) {
-			locFrom = solution.get(j - 1);
-			locTo = solution.get(j);
-			cost += instance.getDistance(locFrom, locTo);
-		}
-		
-		locFrom = solution.get(j);
-		locTo = solution.get((j + 1) % n);
-		cost += instance.getDistance(locFrom, locTo);
-		
-		return cost;
 	}
 }
---
+++ src/polytech/tours/di/parallel/tsp/fourtytwo/ThreadedSolutionFinder.java
@@ -0,0 +1,141 @@
+package polytech.tours.di.parallel.tsp.fourtytwo;
+
+import java.util.Collections;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ThreadLocalRandom;
+
+import polytech.tours.di.parallel.tsp.Instance;
+import polytech.tours.di.parallel.tsp.Solution;
+import polytech.tours.di.parallel.tsp.TSPCostCalculator;
+
+public class ThreadedSolutionFinder implements Runnable {
+	final Instance instance;
+	Solution solution;
+	
+	public ThreadedSolutionFinder(long seed, Instance instance, Solution solution) {
+		//ThreadLocalRandom.current().setSeed(seed); TODO
+		this.instance = instance;
+		this.solution = solution;
+	}
+
+	@Override
+	public void run() {
+		solution.setOF(Double.MAX_VALUE); //No solution so it has the higher cost
+		
+		do {
+			Solution randomSolution = generateRandomSolution();
+			System.out.println("Random: " + randomSolution);
+			Solution currentSolution = localSearch(randomSolution); //TODO: Must be interruptable
+			System.out.println("Locally searched solution: " + currentSolution);
+			
+			if(currentSolution.getOF() < solution.getOF()) {
+				//Copy the elements because the solution reference must not change
+				solution.clear();
+				solution.addAll(currentSolution);
+				solution.setOF(currentSolution.getOF());
+			}
+
+		} while(!Thread.interrupted());
+	}
+	
+	private Solution generateRandomSolution() {
+		Solution solution = new Solution();
+		
+		/* We add all the city (index) to the solution,
+		 * no more things are required. TSPCostCalculator
+		 * do the trick */
+		for(int i = 0; i < instance.getN(); i++) {
+			solution.add(i);
+		}
+		
+		//Randomize the solution indices (cities)
+		Collections.shuffle(solution, ThreadLocalRandom.current());
+
+		//Compute the efficiency of the solution
+		solution.setOF(TSPCostCalculator.calcOF(instance, solution));
+		
+		return solution;
+	}
+
+	/* Return the best solution among many changed solution
+	 * (local search algorithm)
+	 * @param generatedSolution The solution to begin with
+	 * @return the best solution found with the algorithm,
+	 * null if no better solution
+	 */
+	private Solution localSearch(Solution solution) {
+		boolean continueExploration = true;
+		Solution bestSolution = solution;
+		
+		while(continueExploration)
+		{
+			Solution swapSolution;
+			
+			swapSolution = exploreNeighborhood(bestSolution);
+			//TODO: Solve this, computeSwapCost is inaccurate
+			if((float)swapSolution.getOF() < (float)bestSolution.getOF())
+				bestSolution = swapSolution;
+			else
+				continueExploration = false;
+		}
+		
+		return bestSolution;
+	}
+
+	/* Return the best solution among many changed solution
+	 * (local search algorithm)
+	 * @param generatedSolution The solution to begin with
+	 * @return the best solution found with the algorithm
+	 */
+	private Solution exploreNeighborhood(Solution solution) {
+		Solution bestSolution = solution;
+		Solution swapSolution = solution.clone();
+
+		for(int i = 0; i < solution.size(); i++)
+		{
+			for(int j = i + 1; j < solution.size(); j++)
+			{
+				double costBefore = swapSolution.getOF();
+
+				double relativeCostBefore = computeSwapCost(swapSolution, i, j);
+				swapSolution.swap(i, j);
+				double relativeCostAfter = computeSwapCost(swapSolution, i, j);
+				double diffCost = relativeCostBefore - relativeCostAfter;
+				
+				swapSolution.setOF(costBefore - diffCost);
+				//swapSolution.setOF(TSPCostCalculator.calcOF(instance, swapSolution));
+				
+				if(swapSolution.getOF() < bestSolution.getOF()) 
+					bestSolution = swapSolution.clone();
+			}
+		}
+		
+		return bestSolution;
+	}
+	
+	private double computeSwapCost(Solution solution, int i, int j) {
+		double cost;
+		int locFrom, locTo;
+		int n = solution.size();
+		
+		locFrom = solution.get((i - 1 + n) % n);
+		locTo = solution.get(i);
+		cost = instance.getDistance(locFrom, locTo);
+		
+		locFrom = solution.get(i);
+		locTo = solution.get(i + 1);
+		cost += instance.getDistance(locFrom, locTo);
+
+		if(j - i != 1) {
+			locFrom = solution.get(j - 1);
+			locTo = solution.get(j);
+			cost += instance.getDistance(locFrom, locTo);
+		}
+		
+		locFrom = solution.get(j);
+		locTo = solution.get((j + 1) % n);
+		cost += instance.getDistance(locFrom, locTo);
+		
+		return cost;
+	}
+}
